25edb660192cc0521c70f45b45c314d259311654
Murilo Gama <murilo.gama@superlogica.com>
2021-07-15 12:06:39 -0300

    Add wp core and plugins

---
+++ wp-content/plugins/wp-graphql/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v1.5.0',
+        'version' => '1.5.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'aba7bde76f7e0d0be9d0fc1f5991041454fecbb9',
+        'name' => 'wp-graphql/wp-graphql',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ivome/graphql-relay-php' => array(
+            'pretty_version' => 'v0.5.0',
+            'version' => '0.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ivome/graphql-relay-php',
+            'aliases' => array(),
+            'reference' => 'b227c17e79564d147db2162329f5bdbe19192313',
+            'dev_requirement' => false,
+        ),
+        'webonyx/graphql-php' => array(
+            'pretty_version' => 'v14.8.0',
+            'version' => '14.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webonyx/graphql-php',
+            'aliases' => array(),
+            'reference' => '7149ebfb0ba4132ca3cc5ef943b8e2ece58a5d5b',
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wp-graphql' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'aba7bde76f7e0d0be9d0fc1f5991041454fecbb9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
